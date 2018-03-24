2cae00a66e51ff2c39db5de905397027524793b6
SOA-KunLin <orange63@hotmail.com>
2018-03-24 09:01:21 +0000

    C. Advanced Crypto and Hashing

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -3,6 +3,7 @@
 require_relative '../credit_card'
 require_relative '../substitution_cipher'
 require_relative '../double_trans_cipher'
+require_relative '../sk_cipher'
 require 'minitest/autorun'
 
 describe 'Test card info encryption' do
@@ -37,4 +38,47 @@ describe 'Test card info encryption' do
       end
     end
   }
+  
+  describe 'Using Modern Symmetric Cipher' do
+    it 'should generate new Base64 keys' do
+      key = ModernSymmetricCipher.generate_new_key
+      key.wont_be_nil
+    end
+
+    it 'should generate different keys each time' do
+      key1 = ModernSymmetricCipher.generate_new_key
+      key2 = ModernSymmetricCipher.generate_new_key
+      key1.wont_equal key2
+    end
+    
+    it 'should encrypt card information' do
+      key = ModernSymmetricCipher.generate_new_key
+      enc = ModernSymmetricCipher.encrypt(@cc.to_s, key)
+      enc.wont_be_nil
+      enc.wont_equal @cc.to_s
+    end
+    
+    it 'should decrypt encrypted card information' do
+      key = ModernSymmetricCipher.generate_new_key
+      enc = ModernSymmetricCipher.encrypt(@cc.to_s, key)
+      dec = ModernSymmetricCipher.decrypt(enc, key)
+      dec.must_equal @cc.to_s
+    end
+    
+    it 'should encrypted the text using different nonces each time' do
+      key  = ModernSymmetricCipher.generate_new_key
+      enc1 = ModernSymmetricCipher.encrypt(@cc.to_s, key)
+      enc2 = ModernSymmetricCipher.encrypt(@cc.to_s, key)
+      enc1.wont_equal enc2
+    end
+    
+    it 'should recover the same text from ciphertexts with different nonces' do
+      key  = ModernSymmetricCipher.generate_new_key
+      enc1 = ModernSymmetricCipher.encrypt(@cc.to_s, key)
+      enc2 = ModernSymmetricCipher.encrypt(@cc.to_s, key)
+      dec1 = ModernSymmetricCipher.decrypt(enc1, key)
+      dec2 = ModernSymmetricCipher.decrypt(enc2, key)
+      dec1.must_equal dec2
+    end
+  end
 end
